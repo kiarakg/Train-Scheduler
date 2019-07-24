409e218c5ec8e6844bea4bb9e1a364e9c4ff518f
Kiara Guzman <kiara.kg11@gmail.com>
2019-07-24 16:53:07 -0400

    firebase added

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDK77TFhsQjYoCL8MGs_5I2-yb-ifU6otM",
+    authDomain: "train-scheduler-cee10.firebaseapp.com",
+    databaseURL: "https://train-scheduler-cee10.firebaseio.com",
+    projectId: "train-scheduler-cee10",
+    storageBucket: "",
+    messagingSenderId: "316318061062",
+    appId: "1:316318061062:web:213151623011e6e3"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+});
