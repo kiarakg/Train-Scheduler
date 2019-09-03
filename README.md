fa4734aeb65cde76fb727c7d450276cc980de8d4
Kiara Guzman <kiara.kg11@gmail.com>
2019-09-03 14:53:36 -0400

    added js functionality

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Train-Scheduler
+
+
+
+## Description
+The user can enter the name of the train, destination, start time, and frequency. The train information is saved in a database and published to the Current Train Schedule. Also, the frequency and start time are used to calculate the Next Arrival time and Minutes Away, which is then published to the Current Train Schedule.
+
+
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,76 @@
-$(document).ready(function(){
-
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyDK77TFhsQjYoCL8MGs_5I2-yb-ifU6otM",
-    authDomain: "train-scheduler-cee10.firebaseapp.com",
-    databaseURL: "https://train-scheduler-cee10.firebaseio.com",
-    projectId: "train-scheduler-cee10",
-    storageBucket: "",
-    messagingSenderId: "316318061062",
-    appId: "1:316318061062:web:213151623011e6e3"
+var config = {
+    apiKey: "AIzaSyDnVhgA2JmgTRKrIqq0TWCuaDa0O1bAorg",
+    authDomain: "train-scheduler-d55e5.firebaseapp.com",
+    databaseURL: "https://train-scheduler-d55e5.firebaseio.com",
+    projectId: "train-scheduler-d55e5",
+    storageBucket: "train-scheduler-d55e5.appspot.com",
+    messagingSenderId: "128336011606"
   };
-
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
-});
+  
+  firebase.initializeApp(config);
+  
+  var database = firebase.database();
+  
+  $("#train-form").on("submit", function(event) {
+    event.preventDefault();
+  
+    // grab train data input
+    var trainDataInput = {
+      trainName: $("#train-name-input").val().trim(),
+      destination: $("#destination-input").val().trim(),
+      firstTrain: $("#first-train-input").val().trim(),
+      frequency: $("#frequency-input").val().trim()
+    };
+  
+    // add train data to database
+    database.ref().push(trainDataInput);
+  
+    // clear add train form
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#first-train-input").val("");
+    $("#frequency-input").val("");
+  
+  });
+  
+  database.ref().on("child_added", function(childSnapshot) {
+    
+    var trainData = childSnapshot.val();
+  
+    // grab frequency
+    var tFrequency = trainData.frequency;
+  
+    // convert first train time
+    var firstTrainConverted = moment(trainData.firstTrain, "HH:mm").subtract(1, "years");
+  
+    // Current Time
+    var currentTime = moment();
+  
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTrainConverted), "minutes");
+  
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency;
+  
+    // Minute Until Train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+  
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  
+    // create a <tr>
+    var $tr = $("<tr>");
+  
+    // create <td> tag for each column - 5 total
+    // add content from trainData to corresponding <td> tag
+    var $tdName = $("<td>").text(trainData.trainName);
+    var $tdDestination = $("<td>").text(trainData.destination);
+    var $tdFrequency = $("<td>").text(trainData.frequency);
+    var $tdNextArrival = $("<td>").text(moment(nextTrain).format("hh:mm A"));
+    var $tdMinutesAway = $("<td>").text(tMinutesTillTrain);
+  
+    $tr.append($tdName, $tdDestination, $tdFrequency, $tdNextArrival, $tdMinutesAway);
+  
+    $("tbody").append($tr);
+    
+  });
